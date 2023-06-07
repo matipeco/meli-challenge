b4310a805d560cb04536ee64cb77f5d50dbf2995
matipeco <matiaspecorale1995@gmail.com>
2023-06-07 13:32:20 -0300

    instalando vite

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Search } from "./components/Search/index"
+
+function App() {
+  return (
+    <>
+      <Search/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Search/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from "react"
+
+export const Search:FunctionComponent = () =>{
+  return(
+    <input type="text" />
+  )
+} 
