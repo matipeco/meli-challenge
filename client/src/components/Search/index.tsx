3ed066603769a8d3274d6dc8408cc6dfeb9b35d6
matipeco <matiaspecorale1995@gmail.com>
2023-06-09 17:14:42 -0300

    actualizando componente Search

--- client/src/components/Search/index.tsx
+++ client/src/components/Search/index.tsx
@@ -1,20 +1,44 @@
-import { FunctionComponent } from "react"
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from "react"
 import "./style.scss"
 import lupa from "../../assets/ic_Search.png"
 import logo from "../../assets/Logo_ML.png"
 import { Container } from "../Container"
+import { useNavigate } from 'react-router-dom'
 
 export const Search:FunctionComponent = () =>{
+
+  const navigate = useNavigate();
+
+  const [input, setInput] = useState<string>("");
+
+  const handleSubmit = (ev:FormEvent<HTMLFormElement>)=>{
+    if(input === ""){
+      ev.preventDefault();
+      return
+    }
+    ev.preventDefault();
+    navigate(`/items?q=${input}`);
+  }
+
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>)=>{
+    setInput(ev.target.value.toLowerCase())
+  }
   return(
       <header className="header">
         <Container>
         <img className="header__logo" src={logo} alt="" />
-        <input className="header__input" type="text" placeholder="Nunca dejes de buscar"/>
-        <button className="header__button">
-          <img src={lupa} alt="" className="header__button-logo" />
-        </button>
+        <form action="" className="header__form" onSubmit={handleSubmit}>
+          <label htmlFor="input">
+            <input id="input" className="header__input" 
+            type="text" placeholder="Nunca dejes de buscar" 
+            value={input} 
+            onChange={handleChange}/>
+          </label>
+          <button className="header__button">
+            <img src={lupa} alt="" className="header__button-logo" />
+          </button> 
+        </form>
         </Container>
       </header>
-    
   )
 } 
