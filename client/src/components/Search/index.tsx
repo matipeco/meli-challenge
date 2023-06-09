57f4399a7f6e0336e433538ea74877b4adfae7d9
matipeco <matiaspecorale1995@gmail.com>
2023-06-09 12:23:50 -0300

    estilos Search

--- client/src/components/Search/index.tsx
+++ client/src/components/Search/index.tsx
@@ -2,15 +2,18 @@ import { FunctionComponent } from "react"
 import "./style.scss"
 import lupa from "../../assets/ic_Search.png"
 import logo from "../../assets/Logo_ML.png"
+import { Container } from "../Container"
 
 export const Search:FunctionComponent = () =>{
   return(
       <header className="header">
+        <Container>
         <img className="header__logo" src={logo} alt="" />
         <input className="header__input" type="text" placeholder="Nunca dejes de buscar"/>
-        <button>
-          <img src={lupa} alt=""  />
+        <button className="header__button">
+          <img src={lupa} alt="" className="header__button-logo" />
         </button>
+        </Container>
       </header>
     
   )
