29b2528acbbb7be04498e9a5a27ade3f530ea60b
matipeco <matiaspecorale1995@gmail.com>
2023-06-09 17:13:41 -0300

    instalando axios, react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import { Search } from "./components/Search/index"
+import { Home } from "./views/Home";
+import { Products } from "./views/Products";
+import { Detail } from "./views/Detail";
 
 function App() {
+  
   return (
     <>
       <Search/>
-    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/items' element={<Products/>}/>
+        <Route path='/items/:id' element={<Detail/>}/>
+      </Routes> 
     </>
   )
 }
