6b01e6652eb883c3721091f8bc6c2798cef1cb74
matipeco <matiaspecorale1995@gmail.com>
2023-06-13 23:38:29 -0300

    componente Card update

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from "react"
 import { Product } from "../../types"
 import './style.scss'
+import shipping from '../../assets/ic_shipping.png'
+import { Link } from "react-router-dom"
 
 type Props = {
   prod: Product;
@@ -8,9 +10,27 @@ type Props = {
 
 export const Card:FunctionComponent<Props> = ({prod
 })=>{
+
+  const priceFormat = prod.price.amount.toLocaleString('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 0,
+  })
+  
   return(
       <article className="card__container">
-        <h1>{prod.title}</h1>
+        <Link to={`/items/${prod.id}`}>
+          <img className="card__image" src={prod.picture} alt={prod.title}/>
+        </Link>
+        
+        <div className="card__div">
+          <p className="card__contain-price">
+            {priceFormat}
+            <img className="card__contain-shipping" src={shipping} alt="" />  
+          </p>
+          <p className="card__contain-title">{prod.title}</p>
+        </div>
+        <p className="card__location">Capital Federal</p>
       </article>
   )
 }
