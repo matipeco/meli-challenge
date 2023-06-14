5dcd4bdaa6eecc52b003fb56deccf7b96173fcf2
matipeco <matiaspecorale1995@gmail.com>
2023-06-14 20:09:31 -0300

    componente Detail update

--- client/src/views/Detail/index.tsx
+++ client/src/views/Detail/index.tsx
@@ -3,18 +3,20 @@ import { Container } from "../../components/Container"
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { DetailProduct, DetailResponse } from "../../types";
+import './style.scss';
 
 export const Detail = () => {
   const { id } = useParams();
 
   const [product, setProduct] = useState<DetailProduct>();
+  const [categories, setCategories] = useState<string[] | undefined>();
 
   const getDetail = async()=>{
     const { data } = await axios.get<DetailResponse>(`http://localhost:3001/api/items/${id}`);
     setProduct(data.item)
+    setCategories(data.categories)
   }
 
-  
   useEffect(()=>{
     getDetail();
   },[])
@@ -40,16 +42,25 @@ export const Detail = () => {
   return(
     <main>
       <Container>
-        <article>
-          <img src={product.picture} alt="" />
-          <p>{product.condition} - {product.sold_quantity} {product.sold_quantity === 1 ? "vendido" : "vendidos"} </p>
-          <h2>
-            {product.title}
-          </h2>
-          <p>{priceFormat}
-           <sup>{getDecimals(product.price.decimals)}</sup> 
-          </p>
-          <button>Comprar</button>
+        {categories?.map((cat)=>{
+          return <span key={product.id}>{cat}</span>
+        })}
+        <article className="detail__container">
+          
+          <img className="detail__image" src={product.picture} alt={product.title} />
+          
+          <div className="detail__contain">
+            <p className="detail__condition">{product.condition} - {product.sold_quantity} {product.sold_quantity === 1 ? "vendido" : "vendidos"} </p>
+            <h2 className="detail__title">
+              {product.title}
+            </h2>
+            <p className="detail__price">{priceFormat}
+              <sup>{getDecimals(product.price.decimals)}</sup> 
+            </p>
+            <button className="detail__button">Comprar</button>
+          </div>
+          
+          <p>{product.description}</p>
         </article>
       </Container>
     </main>
