602abcb7b1777f5812f20e11d29518782fee488e
matipeco <matiaspecorale1995@gmail.com>
2023-06-16 18:46:53 -0300

    creando componente Home

--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,10 +1,14 @@
 import { Container } from "../../components/Container"
+import './style.scss'
+import landing from '../../assets/meli-background.png'
+// import logo from '../../assets/Logo_ML@2x.png.png.png'
 
 export const Home =()=>{
   return(
-    <main>
+    <main className="landing__container">
       <Container>
-        Home
+          {/* <img className="landing__logo" src={logo} alt="" /> */}
+          <img className="landing__background" src={landing} alt="mercadolibre landing" />
       </Container>
     </main>
   )
