b2f33c566955224f3d2c395e90549afccb2d5289
matipeco <matiaspecorale1995@gmail.com>
2023-06-17 20:06:31 -0300

    agregando estado isLoading

--- client/src/views/Products/index.tsx
+++ client/src/views/Products/index.tsx
@@ -5,39 +5,46 @@ import { SearchResponse } from "../../types";
 import { useLocation } from "react-router-dom";
 import { Card } from "../../components/Card";
 import { Breadcrumb } from "../../components/Breadcrumb";
+import spinner from '../../assets/spinner.svg'
 import './style.scss';
 
-export const Products:FunctionComponent = () => { 
+export const Products: FunctionComponent = () => {
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const q = params.get('q');
 
   const [info, setInfo] = useState<SearchResponse>();
-
-  const getProducts = async() =>{
+  const [isLoading, setIsLoading] = useState<boolean>(true); 
+  
+  const getProducts = async () => {
     try {
       const { data } = await axios.get<SearchResponse>(`http://localhost:3001/api/items?q=${q}`);
       setInfo(data);
+      setIsLoading(false); 
 
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect( ()=>{
+  useEffect(() => {
     getProducts()
-  },[q])
+  }, [q])
 
-  return(
-   <main className="container-cards">
-    <Container>
-        {info?.categories === undefined ? null : <Breadcrumb categories={info?.categories}/> }
-        {info?.items.map((prod)=>{
-        return(
-          <Card prod={prod} key={prod.id}/>
-        )
-      })}
-    </Container>
-   </main> 
+  return (
+    <main className="container-cards">
+      <Container>
+        {isLoading ? (
+          <img src={spinner} alt="" width="40" height="40" />
+        ) : (
+          <>
+            {info?.categories === undefined ? null : <Breadcrumb categories={info?.categories} />}
+            {info?.items.map((prod) => (
+              <Card prod={prod} key={prod.id} />
+            ))}
+          </>
+        )}
+      </Container>
+    </main>
   )
 }
