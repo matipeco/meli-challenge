f51b52b60c787f53eabd56b67e606340d41888ae
matipeco <matiaspecorale1995@gmail.com>
2023-06-27 20:49:57 -0300

    update PageNotFound

--- client/src/components/PageNotFound/index.tsx
+++ client/src/components/PageNotFound/index.tsx
@@ -1,15 +1,12 @@
 import { FunctionComponent } from "react";
 import pageNot from '../../assets/pagenot.svg'
-import { Container } from "../Container";
 import './style.scss'
 
 export const PageNotFound:FunctionComponent = () =>{
   return(
-    <Container>
-      <main className="page-not__main">
+      <div className="page-not__div">
         <img src={pageNot} alt="" width="20" height="20" />
         <p>Parece que esta página no existe...</p>
-      </main>
-    </Container>
+      </div>
   )
 }
